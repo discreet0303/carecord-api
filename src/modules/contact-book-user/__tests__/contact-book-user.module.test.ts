cb66385eaa6c09fd76d98182a639d28baff7d83f
discreet0303 <qhespqhesp@gmail.com>
2023-06-09 21:31:46 +0800

    fix test

--- src/modules/contact-book-user/__tests__/contact-book-user.module.test.ts
+++ src/modules/contact-book-user/__tests__/contact-book-user.module.test.ts
@@ -1,8 +1,17 @@
 import { Test } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { ContactBookUserRelationRepository } from 'src/repositories/contact-book-user-relation.repository';
+import { ContactBookUserRepository } from 'src/repositories/contact-book-user.repository';
+import { dataSourceMockFactory } from 'src/utils/test/typeorm.mock';
 import { ContactBookUserController } from '../contact-book-user.controller';
 import { ContactBookUserService } from '../contact-book-user.service';
 
 export const ContactBookUserTestModule = Test.createTestingModule({
   controllers: [ContactBookUserController],
-  providers: [ContactBookUserService],
+  providers: [
+    ContactBookUserService,
+    { provide: ContactBookUserRelationRepository, useFactory: dataSourceMockFactory },
+    { provide: ContactBookUserRepository, useFactory: dataSourceMockFactory },
+    { provide: DataSource, useFactory: dataSourceMockFactory },
+  ],
 });
